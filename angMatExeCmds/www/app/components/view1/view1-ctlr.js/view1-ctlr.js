63f1d3def401033fa50de88ea820993a9814fbe4
Joe Dorocak <joecodeswell@gmail.com>
2016-01-16 10:43:20 -0800

    compiles but won't fire up 2nd view

---
+++ angMatExeCmds/www/app/components/view1/view1-ctlr.js/view1-ctlr.js
@@ -0,0 +1,56 @@
+"use strict";
+
+angular.module("ngapp").controller("View1Controller", function(shared, $state, $scope, $mdSidenav, $mdComponentRegistry){
+
+    var ctrl = this;
+
+    this.auth = shared.info.auth;
+
+    this.toggle = angular.noop;
+
+    this.title = $state.current.title;
+
+    /*
+    this.isOpen = function() { return false };
+    $mdComponentRegistry
+    .when("left")
+    .then( function(sideNav){
+      ctrl.isOpen = angular.bind( sideNav, sideNav.isOpen );
+      ctrl.toggle = angular.bind( sideNav, sideNav.toggle );
+    });
+
+    this.toggleRight = function() {
+    $mdSidenav("left").toggle()
+        .then(function(){
+        });
+    };
+    // i don't understand this
+    this.close = function() {
+    $mdSidenav("right").close()
+        .then(function(){
+        });
+    };
+    */
+    // joe renames id from left to thesidenav
+    this.isOpen = function() { return false };
+    $mdComponentRegistry
+    .when("thesidenav")
+    .then( function(sideNav){
+      ctrl.isOpen = angular.bind( sideNav, sideNav.isOpen );
+      ctrl.toggle = angular.bind( sideNav, sideNav.toggle );
+    });
+
+    this.toggleRight = function() {
+    $mdSidenav("thesidenav").toggle()
+        .then(function(){
+        });
+    };
+    
+    // i don't understand this
+    this.close = function() {
+    $mdSidenav("thesidenav").close()
+        .then(function(){
+        });
+    };
+
+});
--- angMatExeCmds/www/app/routes.js
+++ angMatExeCmds/www/app/routes.js
@@ -1,15 +1,37 @@
 "use strict";
 
+// modified from http://stackoverflow.com/q/29866119/601770
+// https://github.com/angular-ui/ui-router/wiki/Multiple-Named-Views
+// https://github.com/angular-ui/ui-router/wiki/url-routing
+
 angular.module("ngapp").config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider){
 
     $urlRouterProvider.otherwise("/main");
 
-    $stateProvider.state("main", {
+    $stateProvider.state( 
+    
+    // https://github.com/angular-ui/ui-router/wiki/Multiple-Named-Views
+    "main", {
         url: "/main",
         templateUrl: "app/components/main/main.html",
         title: "Execute Commands - Ang Mat",
         controller: "MainController",
         controllerAs: "main"
-    });
+    },
+    
+    // http://stackoverflow.com/q/29866119/601770
+    // the absolute file address:
+    //     file:///C:/1d/CrossPlatformPjs/CordovaPjs/angMatExeCmdsPj/angMatExeCmds/www/app/components/view1/view1.html
+    
+    "view1", {
+        url: "view1",
+        templateUrl: "app/components/view1/view1.html",
+        title: "Execute Commands - View1",
+        controller: "View1Controller",
+        controllerAs: "view1"
+    }
+    
+    );
+    
 
 }]);
